b6e5730aec7057d8a38af161150ee72c336ee756
Saiful Islam Sagar <46032500+sisagarbd@users.noreply.github.com>
2023-10-28 04:27:57 +0600

    DOM select

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("hello world!");
+// document.getElementById
+
+let val;
+
+val = document.getElementById('document-title');
+val = document.getElementById('document-title').id;
+val = document.getElementById('document-title').className;
+
+document.getElementById('document-title').style.background = '#333';
+document.getElementById('document-title').style.color = '#ffffff';
+document.getElementById('document-title').style.paddingLeft = '10px';
+document.getElementById('document-title').innerText = 'Hello';
+document.getElementById('document-title').innerHTML = '<i>Hello</i>';
+
+
+val = document.getElementById('document-title');
+
+val.innerText = "ha ha ha haaaa....";
+
+
+val = document.querySelector('ul li');
+
+console.log(val);
