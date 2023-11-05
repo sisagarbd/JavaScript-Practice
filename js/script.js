2a9abc96e6a98cd2d7f47ede806de97b88ccf4d8
Saiful Islam Sagar <46032500+sisagarbd@users.noreply.github.com>
2023-11-06 02:43:22 +0600

    Promise & Api

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-document.getElementById('get_data').addEventListener('click',loadData);
+document.getElementById("get_data").addEventListener("click", getData);
 
-function loadData() {
-
-  let xhr = new XMLHttpRequest();
-  console.log(xhr);
+function getData(data) {
+  fetch('https://api.chucknorris.io/jokes/random')
+  
+  .then(function(response) {
+  return response.json();
+  })
+  .then(function(data) {
+  console.log(data);
+  })
+.catch(function(err) {
+  console.log(err);
+  });
 
 }
+
+
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(err => console.log(err));
